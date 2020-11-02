c708949c266844c4b6530cf3bb3a35a89398364b
Hu Jindi <hujindi1212@gmail.com>
2020-11-02 15:11:17 +0800

    Update model.py

--- model.py
+++ model.py
@@ -6,12 +6,12 @@ def get_data():
     mat = sio.loadmat('./data/cars_annos.mat')
     annotations = mat["annotations"]
     _, total_size = annotations.shape
-    dataset = np.zeros((total_size, 5))
+    dataset = np.zeros((total_size, 6))
     for i in range(total_size):
         img_path = annotations[:, i][0][0][0]
         img_name = img_path.split(".")[0][8:]
         img_id = int(img_name) - 1
-        for j in range(5):
+        for j in range(6):
             dataset[img_id, j] = int(annotations[:, i][0][j + 1][0])
     return dataset
 
