447c66e19412c017583f9569b4353a3f7cca0a10
Hu Jindi <hujindi1212@gmail.com>
2020-11-02 15:10:17 +0800

    car imgs

--- model.py
+++ model.py
@@ -1,5 +1,20 @@
 import scipy.io as sio
+import numpy as np
 
-mat = sio.loadmat('data/cars_annos.mat')
 
-print(mat['annotations'])
+def get_data():
+    mat = sio.loadmat('./data/cars_annos.mat')
+    annotations = mat["annotations"]
+    _, total_size = annotations.shape
+    dataset = np.zeros((total_size, 5))
+    for i in range(total_size):
+        img_path = annotations[:, i][0][0][0]
+        img_name = img_path.split(".")[0][8:]
+        img_id = int(img_name) - 1
+        for j in range(5):
+            dataset[img_id, j] = int(annotations[:, i][0][j + 1][0])
+    return dataset
+
+
+data_arr = get_data()
+print(data_arr[:5])
