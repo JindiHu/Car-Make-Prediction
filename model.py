6a2b8b85a18320a78492820fa0e5dcea32590e5c
Hu Jindi <hujindi1212@gmail.com>
2020-11-02 22:56:14 +0800

    Update model.py

--- model.py
+++ model.py
@@ -7,31 +7,27 @@ import matplotlib.image as mpimg
 
 
 def load_data():
+    print('loading images ... ')
     mat = sio.loadmat('./data/cars_annos.mat')
     annotations = mat["annotations"]
     _, total_size = annotations.shape
-    dataset = np.zeros((total_size, 6))
+    images_rgb = []
+    data_labels = np.zeros((total_size, 6))
     for i in range(total_size):
-        img_path = annotations[:, i][0][0][0]
-        img_name = img_path.split(".")[0][8:]
-        img_id = int(img_name) - 1
+        image_path = annotations[:, i][0][0][0]
+        image_rgb = mpimg.imread("./data/" + image_path)
+        images_rgb.append(image_rgb)
         for j in range(6):
-            dataset[img_id, j] = int(annotations[:, i][0][j + 1][0])
-
-    return dataset
-
-
-def peek_image(path, idx, dataset):
-    files = os.listdir("./data/" + path)
-    img = mpimg.imread("./data/" + path + "/" + files[idx])
-    print("image is", files[idx])
-    img_name, extension = files[idx].split('.')
-    print(img_name)
-    print("the label is " + str(dataset[int(img_name[0]) - 1, 4]))
-    plt.imshow(img)
-    plt.show()
+            data_labels[i, j] = int(annotations[:, i][0][j + 1][0])
+        if (i + 1) % 1000 == 0:
+            print('{:>6} images loaded'.format(str(i + 1)))
+        if i == 999:
+            break
+    images_rgb = np.array(images_rgb, dtype=object)
+    print('images loaded!')
+    return images_rgb, data_labels
 
 
-data_arr = load_data()
-peek_image("car_ims", 0, data_arr)
-print(data_arr[:5])
+images, labels = load_data()
+print(images)
+print(labels)
