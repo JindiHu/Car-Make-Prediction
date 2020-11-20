86ce860f61d024ffee845c92b0f4f2fc0fa7808d
Hu Jindi <hujindi1212@gmail.com>
2020-11-21 00:07:44 +0800

    Update model.py

--- model.py
+++ model.py
@@ -4,7 +4,6 @@ import scipy.io as sio
 import numpy as np
 import tensorflow.keras as keras
 import matplotlib.pyplot as plt
-from PIL import Image
 from tensorflow.keras import layers
 
 seed = 0
@@ -45,8 +44,6 @@ def load_data():
     train_y = []
     test_x = []
     test_y = []
-    # images_rgb = []
-    # data_labels = []
     count = 1
     for i in range(total_size):
         bbox_x1 = annotations[:, i][0][1][0][0]
@@ -57,19 +54,15 @@ def load_data():
         is_test = annotations[:, i][0][6][0][0]
         # print(bbox_x1, bbox_y1, bbox_x2, bbox_y2)
         image_path = annotations[:, i][0][0][0]
-        # img = Image.open("./data/" + image_path)
         img = tf.keras.preprocessing.image.load_img("./data/" + image_path)
         img = tf.keras.preprocessing.image.img_to_array(img) / 255
         img = tf.image.crop_to_bounding_box(img, offset_height=bbox_y1, offset_width=bbox_x1,
                                             target_height=(bbox_y2 - bbox_y1), target_width=(bbox_x2 - bbox_x1))
-        img = tf.image.resize_with_pad(img, target_height=128, target_width=128, antialias=True)
-        # if img.mode == 'RGB':
-        #     h_resize = 128
-        #     w_resize = 128
-        # cropped_img = img.crop((bbox_x1, bbox_y1, bbox_x2, bbox_y2))
-        # resized_img = np.array(cropped_img.resize((w_resize, h_resize), Image.ANTIALIAS))
-        # cropped_img.thumbnail((w_resize, h_resize), Image.ANTIALIAS)
-        # resized_img = np.array(cropped_img)
+        # img = tf.image.resize_with_pad(img, target_height=128, target_width=128, antialias=True)
+        img = tf.image.resize(img, size=(128, 128), antialias=True)
+        img = tf.make_tensor_proto(img)
+        img = tf.make_ndarray(img)
+        # print(img)
         if is_test == 0:
             train_x.append(img)
             train_y.append(class_label)
@@ -78,34 +71,31 @@ def load_data():
             test_y.append(class_label)
         if count % 1000 == 0:
             print('{:>6} images loaded'.format(str(count)))
-        if count == 100:
+        if count == 1000:
             break
         count += 1
     print('{:>6} images for training'.format(len(train_x)))
     print('{:>6} images for testing'.format(len(test_x)))
-
-    # train_x = train_x
-    print('test')
-    # train_x = tf.image.resize(train_x, size=(128, 128))
-    train_y = np.array(train_y)
-    # test_x = np.array(test_x)
-    test_y = np.array(test_y)
+    # train_x.numpy()
+    train_x = tf.convert_to_tensor(train_x, dtype=tf.dtypes.float32)
+    train_y = tf.convert_to_tensor(train_y)
+    train_y = train_y - 1
+    test_x = tf.convert_to_tensor(test_x, dtype=tf.dtypes.float32)
+    test_y = tf.convert_to_tensor(test_y)
+    test_y = test_y - 1
     print('images loaded!')
     return train_x, train_y, test_x, test_y
 
 
 def make_car_make_prediction_model(num_ch_c1, num_ch_c2, use_dropout=False):
     model = tf.keras.Sequential()
-    # model.add(layers.Input(shape=(3072,)))
-    # model.add(layers.Reshape(target_shape=(32, 32, 3), input_shape=(3072,)))
-    # model.add(
-    #     layers.Conv2D(num_ch_c1, 9, activation='relu', padding="VALID", use_bias=True, input_shape=(32, 32, 3)))
-    # model.add(layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding='VALID', input_shape=(24, 24, 50)))
-    # model.add(layers.Conv2D(num_ch_c2, 5, activation='relu', padding="VALID", use_bias=True, input_shape=(12, 12, 50)))
-    # model.add(layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding='VALID', input_shape=(8, 8, 60)))
-    # model.add(layers.Flatten())
-    # model.add(layers.Dense(300, use_bias=True))
-    # model.add(layers.Dense(10, use_bias=True))
+    model.add(layers.Conv2D(num_ch_c1, 3, activation='relu', padding="VALID", use_bias=True))
+    model.add(layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding='VALID'))
+    model.add(layers.Conv2D(num_ch_c2, 5, activation='relu', padding="VALID", use_bias=True))
+    model.add(layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding='VALID'))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(300, use_bias=True))
+    model.add(layers.Dense(196, use_bias=True))
     return model
 
 
@@ -115,10 +105,13 @@ def main():
 
     train_x, train_y, test_x, test_y = load_data()
     # print(train_x[45])
-    plt.imshow(train_x[45])
-    print(train_y[45])
+    plt.imshow(train_x[0])
+    print(train_x.shape)
+    print(train_y)
+    print(train_y.shape)
     plt.show()
     # print(images.shape)
+
     # print(labels.shape)
 
     if optimizer_ == 'SGD':
@@ -131,6 +124,15 @@ def main():
         raise NotImplementedError('Complete it by yourself')
     else:
         raise NotImplementedError(f'You do not need to handle [{optimizer_}] in this project.')
+
+    model.compile(optimizer=optimizer, loss=loss, metrics='accuracy')
+    history = model.fit(
+        train_x,
+        train_y,
+        batch_size=batch_size,
+        epochs=epochs,
+        validation_data=(test_x, test_y))
+    print(history)
 
     if not os.path.exists('./models'):
         os.mkdir('./models')
